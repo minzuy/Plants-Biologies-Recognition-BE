c0fb35bb983e953ca0c880f3126b1e5ffdba297a
minzuy <duybmse18042@fpt.edu.vn>
2025-06-25 22:14:27 +0700

    Update Dockerfile and other changes for Render deployment

--- PlantBiologyEducation/obj/Debug/net8.0/PlantBiologyEducation.AssemblyInfo.cs
+++ PlantBiologyEducation/obj/Debug/net8.0/PlantBiologyEducation.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PlantBiologyEducation")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+73c9b356c74abc57aecc7cf47282a770c552bf47")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+012ac4f870b3f56029b7b09cea95944c6f4c2e77")]
 [assembly: System.Reflection.AssemblyProductAttribute("PlantBiologyEducation")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PlantBiologyEducation")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
